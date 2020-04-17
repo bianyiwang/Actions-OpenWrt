1eab74c17e6f2abbde173efdd177a3136b114354
bianyiwang <63838153+bianyiwang@users.noreply.github.com>
2020-04-17 14:50:14 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,6 +6,5 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate'
 SED -i ' S / 192.168.1.1 / 192.168.0.10 /g'包/基层-文件/文件/ bin中/ config_generate
-
